22c2f902c7c26fe771ac1f546b6acbea6c02e9d3
이연희 <ydol2@naver.com>
2022-06-02 17:03:35 +0900

    2022-06-02 modified

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long,Member> store = new HashMap<>(); //저장소니까 Map이 있어야함. 이유는 뭐징!
     @Override
